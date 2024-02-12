ef4dddb85208dca06ca63c574ddd34caa22a71b6
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-12 18:11:52 +0100

    hechos

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import NetLInk from './components/NetLink.vue'
+</script>
+
+<template>
+<Header></Header>
+<NetLInk></NetLInk>
+ </template>
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">Hola</h1>
+  </div>
+</template>
+</style>
