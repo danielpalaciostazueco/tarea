3a6ef524188864d507fd77219364632972f8548f
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-19 18:21:52 +0100

    Añadido Vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 
 createApp(App).mount('#app')
